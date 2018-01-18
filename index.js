38c8c279315cb86c75a98c7a151585906dad12ef
kessler <yanivk@gmail.com>
2018-01-18 03:22:54 +0200

    remove all one-line if/for statements, migrate to ES6

--- index.js
+++ index.js
@@ -1,200 +1,202 @@
-function Node(data) {
-	this.data = data
+class Node {
+	constructor(data) {
+		this.data = data
+	}
+}
+
+class LinkedList {
+	constructor() {
+		this.head = undefined
+		this.tail = undefined
+		this.length = 0
+		this.foreach = this.forEach
+	}
+
+	push(item) {
+		let node = new Node(item)
+
+		if (this.length > 0) {
+			this.tail.next = node
+			node.prev = this.tail
+			this.tail = node
+		} else {
+			this.head = node
+			this.tail = node
+		}
+
+		this.length++;
+
+		return node
+	}
+
+	pushAll(arr) {
+		for (let i = 0; i < arr.length; i++) {
+			this.push(arr[i])
+		}
+	}
+
+	unshift(item) {
+		let node = new Node(item)
+
+		if (this.length > 0) {
+			this.head.prev = node
+			node.next = this.head
+			this.head = node
+		} else {
+			this.head = node
+			this.tail = node
+		}
+
+		this.length++;
+
+		return node
+	}
+
+	unshiftAll(arr) {
+		for (let i = 0; i < arr.length; i++) {
+			this.unshift(arr[i])
+		}
+	}
+
+	forEach(fn) {
+		let current = this.head
+		while (current) {
+			let next = current.next
+			let stop = fn(current)
+			if (stop) break
+			current = next
+		}
+	}
+
+	nodeIterator() {
+		return () => {
+			if (this.head) {
+				let t = this.head
+				this.head = this.head.next
+				return t
+			}
+		}
+	}
+
+	iterator() {
+		return () => {
+			if (this.head) {
+				let data = this.head.data
+				this.head = this.head.next
+				return data
+			}
+		}
+	}
+
+	findFirst(data) {
+		let current = this.head
+
+		while (current !== undefined) {
+			if (current.data === data) {
+				return current
+			}
+
+			current = current.next
+		}
+	}
+
+	findAll(data) {
+		let current = this.head
+		let results = []
+
+		while (current !== undefined) {
+			if (current.data === data) {
+				results.push(current)
+			}
+
+			current = current.next
+		}
+
+		return results
+	}
+
+	findFirstBy(predicate) {
+		let current = this.head
+
+		while (current !== undefined) {
+			if (predicate(current.data)) {
+				return current
+			}
+
+			current = current.next
+		}
+	}
+
+	findAllBy(predicate) {
+		let current = this.head
+		let results = []
+
+		while (current !== undefined) {
+			if (predicate(current.data)) {
+				results.push(current)
+			}
+
+			current = current.next
+		}
+
+		return results
+	}
+
+	remove(node) {
+
+		if (!(node instanceof Node)) {
+			throw new TypeError('remove() can only be used with node objects return from various methods of the list')
+		}
+
+		if (this.length === 0) {
+			return
+		}
+
+		if (this.head === node) {
+			// its the head
+			this.head = this.head.next
+			if (this.head) {
+				this.head.prev = undefined
+
+				if (!this.head.next) {
+					this.tail = this.head // only 1 item remains
+				}
+
+			} else {
+				this.tail = undefined // was the last, now its empty - so clean tail reference too
+			}
+
+			this.length--
+
+		} else if (this.tail === node) {
+			this.tail = this.tail.prev
+			this.tail.next = undefined
+			this.length--
+		} else if (node.prev && node.next) {
+			let prev = node.prev
+			let next = node.next
+			prev.next = next
+			next.prev = prev
+			this.length--
+		}
+
+		node.prev = undefined
+		node.next = undefined
+
+		return node.data
+	}
+
+	shift() {
+		if (this.head) {
+			return this.remove(this.head)
+		}
+	}
+
+	pop() {
+		if (this.tail) {
+			return this.remove(this.tail)
+		}
+	}
 }
 
 module.exports = LinkedList
-
-function LinkedList() {
-	this.head = undefined
-	this.tail = undefined
-	this.length = 0
-}
-
-LinkedList.prototype.push = function(item) {
-	var node = new Node(item)
-
-	if (this.length > 0) {
-		this.tail.next = node
-		node.prev = this.tail
-		this.tail = node
-	} else {
-		this.head = node
-		this.tail = node
-	}
-
-	this.length++
-
-	return node
-}
-
-LinkedList.prototype.pushAll = function(arr) {
-	for (var i = 0; i < arr.length; i++)
-		this.push(arr[i])
-}
-
-LinkedList.prototype.unshift = function(item) {
-	var node = new Node(item)
-
-	if (this.length > 0) {
-		this.head.prev = node
-		node.next = this.head
-		this.head = node
-	} else {
-		this.head = node
-		this.tail = node
-	}
-
-	this.length++
-
-	return node
-}
-
-LinkedList.prototype.unshiftAll = function(arr) {
-	for (var i = 0; i < arr.length; i++)
-		this.unshift(arr[i])
-}
-
-LinkedList.prototype.foreach = LinkedList.prototype.forEach = function(fn) {
-	var current = this.head
-	while (current) {
-		var next = current.next
-		var stop = fn(current)
-		if (stop)
-			break
-		current = next
-	}
-}
-
-LinkedList.prototype.nodeIterator = function() {
-	var next = this.head
-
-	return function() {
-
-		if (next) {
-			var t = next
-			next = next.next
-			return t
-		}
-	}
-}
-
-LinkedList.prototype.iterator = function() {
-	var next = this.head
-
-	return function() {
-		if (next) {
-			var data = next.data
-			next = next.next
-			return data
-		}
-	}
-}
-
-LinkedList.prototype.findFirst = function(data) {
-	var current = this.head
-
-	while (current !== undefined) {
-		if (current.data === data) {
-			return current
-		}
-
-		current = current.next
-	}
-
-	return undefined
-}
-
-LinkedList.prototype.findAll = function(data) {
-	var current = this.head
-	var results = []
-
-	while (current !== undefined) {
-		if (current.data === data) {
-			results.push(current)
-		}
-
-		current = current.next
-	}
-
-	return results
-}
-
-LinkedList.prototype.findFirstBy = function(predicate) {
-	var current = this.head
-	
-	while (current !== undefined) {
-		if (predicate(current.data)) {
-			return current
-		}
-
-		current = current.next
-	}
-
-	return undefined
-}
-
-LinkedList.prototype.findAllBy = function(predicate) {
-	var current = this.head
-	var results = []
-
-	while (current !== undefined) {
-		if (predicate(current.data)) {
-			results.push(current)
-		}
-
-		current = current.next
-	}
-
-	return results
-}
-
-LinkedList.prototype.remove = function(node) {
-
-	if (!(node instanceof Node))
-		throw new TypeError('remove() can only be used with node objects return from various methods of the list')
-
-	if (this.length === 0)
-		return
-
-	if (this.head === node) {
-		// its the head
-		this.head = this.head.next
-		if (this.head) {
-			this.head.prev = undefined
-
-			if (!this.head.next)
-				this.tail = this.head // only 1 item remains
-
-		} else {
-			this.tail = undefined // was the last, now its empty - so clean tail reference too
-		}
-
-		this.length--
-
-	} else if (this.tail === node) {
-		this.tail = this.tail.prev
-		this.tail.next = undefined
-		this.length--
-	} else if (node.prev && node.next) {
-		var prev = node.prev
-		var next = node.next
-		prev.next = next
-		next.prev = prev
-		this.length--
-	}
-
-	node.prev = undefined
-	node.next = undefined
-
-	return node.data
-}
-
-LinkedList.prototype.shift = function() {
-	if (this.head)
-		return this.remove(this.head)
-}
-
-LinkedList.prototype.pop = function() {
-	if (this.tail)
-		return this.remove(this.tail)
-}
